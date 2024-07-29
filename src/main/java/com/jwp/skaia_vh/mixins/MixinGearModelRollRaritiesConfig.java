beca64408bb898a95e1f63efdf7d460a1fc09bc4
JoshWannaPaas <paasjoshua@yahoo.com>
2024-07-28 19:04:05 -0700

    Renamed Mixin File
    MixinGearRollRaritiesConfig -> MixinGearModelRollRaritiesConfig

--- src/main/java/com/jwp/skaia_vh/mixins/MixinGearModelRollRaritiesConfig.java
+++ src/main/java/com/jwp/skaia_vh/mixins/MixinGearModelRollRaritiesConfig.java
@@ -2,9 +2,10 @@ package com.jwp.skaia_vh.mixins;
 
 import com.google.gson.annotations.Expose;
 import com.jwp.skaia_vh.items.gear.VaultDaggerItem;
+import com.jwp.skaia_vh.models.Daggers;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import com.jwp.skaia_vh.models.Daggers;
+import iskallia.vault.config.Config;
 import iskallia.vault.config.GearModelRollRaritiesConfig;
 import iskallia.vault.gear.VaultGearRarity;
 import net.minecraft.resources.ResourceLocation;
@@ -15,27 +16,30 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Mixin(value = GearModelRollRaritiesConfig.class, remap = false)
-public class MixinGearRollRaritiesConfig {
+public abstract class MixinGearModelRollRaritiesConfig extends Config {
+
     @Expose
-    private static Map<VaultGearRarity, List<String>> DAGGER_MODEL_ROLLS;
+    Map<String, List<String>> DAGGER_MODEL_ROLLS;
+
 
     @Inject(method = "reset", at = @At("HEAD"))
     private void resetHook(CallbackInfo ci) {
         DAGGER_MODEL_ROLLS = new HashMap<>();
-        DAGGER_MODEL_ROLLS.put(VaultGearRarity.SCRAPPY, (List<String>) Daggers.REGISTRY
-                .getIds().stream()
-                .map(ResourceLocation::toString)
-                .collect(Collectors.toList()));
+        DAGGER_MODEL_ROLLS.put(VaultGearRarity.SCRAPPY.name(),
+                Daggers.REGISTRY.getIds().stream()
+                    .map(ResourceLocation::toString)
+                    .collect(Collectors.toList()));
     }
 
     @Inject(method = "getRolls", at = @At("HEAD"), cancellable = true)
-    private void getRollsHook(CallbackInfoReturnable<Map<VaultGearRarity, List<String>>> cir, @Local LocalRef<ItemStack> stack) {
+    private void getRollsHook(CallbackInfoReturnable<Map<String, List<String>>> cir, @Local LocalRef<ItemStack> stack) {
         if (stack instanceof VaultDaggerItem)
             cir.setReturnValue(DAGGER_MODEL_ROLLS);
     }
